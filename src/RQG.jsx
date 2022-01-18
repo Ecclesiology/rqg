7943fce06c9907f0b062551f2ec7d8e4d0e9b7b8
John Matthews <JMMatthews@Protonmail.com>
2022-01-18 13:00:46 -0600

    Import dependencies and project code

---
+++ src/RQG.jsx
@@ -0,0 +1,87 @@
+import "./RQG.css"
+const React = require("react");
+require("bootstrap");
+const ReactDOM = require("react-dom");
+const $ = require("jquery");
+const anime = require("animejs");
+
+const Jesus = document.getElementById("Jesus")
+, Einstein = document.getElementById("einstein")
+, Martin = document.getElementById("martin")
+, Daniel = document.getElementById("Daniel"),
+svgArray = [Jesus, Daniel, Martin, Einstein];
+
+var result;
+var svgId;
+var gId;
+var twitterBtn;
+var twitterLink;
+
+$(document).ready(function(){
+  $("#new-quote").click(function(){
+    for(let i = svgArray.length - 1; i > 0; i--){
+  const j = Math.floor(Math.random() * i)
+  const temp = svgArray[i]
+  svgArray[i] = svgArray[j]
+  svgArray[j] = temp
+  result = temp
+}
+    $("svg").detach();
+    $("#App").append(result);
+
+    switch(result){
+      case Jesus:
+        svgId = "#Jesus";
+        gId = "#gJesus";
+        break;
+      case Daniel:
+        svgId = "#Daniel";
+        gId = "#gDaniel";
+        break;
+      case Martin:
+        svgId = "#martin";
+        gId = "#gMartin";
+        break;
+      case Einstein:
+        svgId = "#einstein";
+        gId = "#gEinstein";
+        break;
+    }
+
+    anime({
+  targets: svgId + " path",
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 1000,
+  delay: function(path, i) { return i * 100 },
+  direction: 'normal'
+});
+ console.log(gId)
+ return gId;
+  });
+});
+
+$(document).ready(function(){
+  let result;
+  $("#tweet-quote").click(function(){
+    let postTitle = $(`${gId}`).attr("aria-label");
+    let twitterLink = `https://twitter.com/intent/tweet?text=${postTitle}`
+    $(document).find("a").attr("href", `${twitterLink}`);
+  });
+});
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+    <div id = "quote-box" class="position-absolute top-50 start-50 translate-middle border border-info rounded-3">
+        <div id = "text" class="position-absolute start-50 top-50 translate-middle-x">
+        </div>
+        <div id = "author" class="position-absolute start-50 top-50 translate-middle"></div>
+        <button id="new-quote" class = "btn position-absolute top-100 start-50 translate-middle">New Quote</button>
+        <a id="tweet-quote" href="" class = "btn position-absolute top-0 start-50 translate-middle" target="_blank">Tweet Me!</a>
+    </div>
+    );
+  }
+};
+
+export default MyComponent;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './RQG.css';
+import MyComponent from './RQG';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyComponent />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('App')
 );
 
 // If you want to start measuring performance in your app, pass a function
